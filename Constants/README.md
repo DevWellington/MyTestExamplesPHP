40b47b9dbc3600f2492c9cc212f80f5392ec9525
Wellington Ribeiro <devwellington@gmail.com>
2014-10-02 00:46:40 -0300

    First examples: Constants (PHP 5.x and 5.6)

---
+++ Constants/README.md
@@ -0,0 +1,5 @@
+###Exemplo de uso de Constants:
+
+*Fonte:* 
+http://php.net/manual/pt_BR/language.constants.php
+http://php.net/manual/fr/migration56.new-features.php
---
+++ Constants/index.php
@@ -0,0 +1,15 @@
+<?php
+
+define('NOME', 'Wellington Ribeiro');
+echo NOME;
+
+const SOBRE_NOME = 'Ribeiro';
+echo SOBRE_NOME;
+
+// Examples in PHP 5.6
+const QTDE = 2;
+const VALOR = 200;
+
+const VALOR_FINAL = QTDE * VALOR;
+
+echo VALOR_FINAL;
